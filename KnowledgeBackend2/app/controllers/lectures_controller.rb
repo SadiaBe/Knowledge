8d4d86d58366c2e374cb2951f2267f057b8e1d7d
SadiaBe <sadia.islam.badhon@gmail.com>
2020-08-18 19:54:56 -0400

    added index and show to all controllers

--- KnowledgeBackend2/app/controllers/lectures_controller.rb
+++ KnowledgeBackend2/app/controllers/lectures_controller.rb
@@ -1,2 +1,12 @@
 class LecturesController < ApplicationController
+
+    def index
+        @lectures = Lecture.all
+        render json: @lectures
+    end
+    
+    def show
+        @lecture = Lecture.find(params[:id])
+        render json: @lecture
+    end
 end
