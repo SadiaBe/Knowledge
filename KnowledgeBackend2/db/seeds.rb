2ca51a43629ab11b34aaaf89d1ed2b76fd833469
SadiaBe <sadia.islam.badhon@gmail.com>
2020-08-18 19:41:20 -0400

    seeded data

--- KnowledgeBackend2/db/seeds.rb
+++ KnowledgeBackend2/db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# teachers = Teacher.create([
+#     {name:"Fuad" ,subject:"Computer Science", years:15},
+#     {name:"Beauty" ,subject:"Home", years:10},
+#     {name:"Lina" ,subject:"Games", years:1}
+# ])
+
+# students = Student.create([
+#     {name:"Arfan" ,major:"Games", age:9},
+#     {name:"Sajid" ,major:"Love", age:1},
+#     {name:"Sifat" ,major:"Games", age:7},
+#     {name:"Sadia" ,major:"Computer Science", age:24},
+#     {name:"Fabia" ,major:"Art", age:13},
+#     {name:"Bob" ,major:"Home", age:41}
+# ])
+
+# lectures = Lecture.create([
+    
+#     {teacher_id:1, student_id:1, topic:"Intro to CompSci"},
+#     {teacher_id:3, student_id:1, topic:"Playing Video games"},
+#     {teacher_id:2, student_id:5, topic:"Intro to Cooking "},
+#     {teacher_id:3, student_id:5, topic:"Intro to Art"},
+#     {teacher_id:3, student_id:3, topic:"Lets Play"},
+#     {teacher_id:3, student_id:3, topic:"Playing with cars"},
+#     {teacher_id:1, student_id:4, topic:"Ruby"},
+#     {teacher_id:1, student_id:4, topic:"Ruby on Rails"},
+#     {teacher_id:1, student_id:4, topic:"Learn JavaScript"},
+#     {teacher_id:3, student_id:2, topic:"Playing with toys"},
+#     {teacher_id:2, student_id:6, topic:"Cleaning"}
+# ])
+
