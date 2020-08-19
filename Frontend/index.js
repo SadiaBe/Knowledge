17b936677785bbd18e4196040c039a03b25ecf11
SadiaBe <sadia.islam.badhon@gmail.com>
2020-08-18 20:53:38 -0400

    fetched all instances of teacher and students and display to frontend

---
+++ Frontend/index.js
@@ -0,0 +1,56 @@
+teachersList = document.querySelector("#teachers")
+studentsList = document.querySelector("#students")
+
+// console.log(teachersList)
+
+
+
+teachersUrl = "http://localhost:3000/teachers"
+studentsUrl = "http://localhost:3000/students"
+lecturesUrl = "http://localhost:3000/lectures"
+fetch(teachersUrl)
+.then(r =>r.json())
+.then( (teachers) =>
+teachers.forEach((teacher) => {
+    displayTeacher(teacher)
+  }) 
+)
+
+fetch(studentsUrl)
+.then(r =>r.json())
+.then( (students) =>
+students.forEach((student) => {
+    displayStudent(student)
+  }) 
+)
+
+// fetch(lecturesUrl)
+// .then(r =>r.json())
+// .then( (lectures) =>
+// lectures.forEach((lecture) => {
+//     displayLecture(lecture)
+//   }) 
+// )
+
+
+function displayTeacher(teacher){
+    let teacherName = document.createElement("li")
+    teacherName.innerText = "Name:" + " " +`${teacher.name}`
+
+
+    // let teacherSubject = document.createElement("li")
+    teachersList.append(teacherName)
+}
+
+function displayStudent(student){
+    
+    let studentName = document.createElement("li")
+    studentName.innerText = "Name:" + " " +`${student.name}`
+
+    let studentMajor = document.createElement("p")
+    studentMajor.innerText ="Major:" + " " + `${student.major}`
+
+    
+    // let teacherSubject = document.createElement("li")
+    studentsList.append(studentName, studentMajor)
+}
